3815f74114f7a4cff1023e456473b4a579cdf10e
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-09-11 01:51:47 -0500

    Implements captureManager

---
+++ managers.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy
+import time
+
+class CaptureManager(object):
+
+    def __init__(self, capture, previewWindowManager= None, shouldMirrorPreview = False):
+
+        self.previewWindowManager = previewWindowManager
+        self.shouldMirrorPreview = shouldMirrorPreview
+
+        self._capture = capture
+        self._channel = 0
+        self._enteredFrame = False
+        self._frame = None
+        self._imageFilename = None
+        self._videoFilename = None
+        self._videoEnconding = None
+        self._videoWriter = None
+
+
+        self._startTime = None
+        self._framesElapsed = long(0)
+        self._fpsEstimate = None
+
+    @property
+    def channel(self):
+        return self._channel
+
+    @channel.setter
+    def channel(self, value):
+        if self._channel != value:
+            self._channel = value
+            self._frame = None
