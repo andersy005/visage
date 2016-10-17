8e20a6d7fe2e2851059a8b51b058e5f1219ddf4d
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-10-17 14:51:25 -0500

    Creates  a lookup array for a given function and apply the lookup array to another array

--- utils.py
+++ utils.py
@@ -26,3 +26,35 @@ def createCurveFunc(points):
         kind = 'cubic'
 
     return scipy.interpolate.interp1d(xs, ys, kind, bounds_error= False)
+
+
+
+def createLookupArray(func, length = 256):
+    """Return a lookup for a whole number inputs to a function
+    
+        The lookup values are clamped to [0, length - 1]
+        
+        """
+
+        if func is None:
+            return None
+
+        lookupArray = numpy.empty(length)
+        i = 0
+
+        while i < length:
+            func_i = func(i)
+            lookupArray[i] = min( max(0, func_i), length - 1)
+            i += 1
+
+        return lookupArray
+
+
+def applyLookupArray(lookupArray,src, dst):
+    """Map a source to a destination using a lookup."""
+
+    if lookupArray is None:
+        return 
+
+    dst[:] = lookupArray[src]
+
