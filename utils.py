862a62347e60aa91a3c2cf67d674ab371d3ede9a
Anderson Banihirwe <axbanihirwe@ualr.edu>
2016-10-17 14:41:49 -0500

    :tada: Formulates a curve by converting control points to a function

--- utils.py
+++ utils.py
@@ -2,3 +2,27 @@ import cv2
 import numpy as np
 import scipy.interpolate
 
+
+def createCurveFunc(points):
+    """Return a function derived from control points."""
+
+    if points is None:
+        return None
+
+    numPoints = len(points)
+    if numPoints < 2:
+        return None
+    """function takes an array of (x, y) pairs
+        treat x as a channel's input value
+        and y as the corresponding output value. For example, (128, 160) would
+        brighten a channel's midtones."""
+    xs, ys = zip(*points)
+
+    if numPoints < 4:
+        kind = 'linear'
+        # quadratic is not implemented
+
+    else:
+        kind = 'cubic'
+
+    return scipy.interpolate.interp1d(xs, ys, kind, bounds_error= False)
